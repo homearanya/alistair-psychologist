7919bb58a438718f69996554293f047f34027481
Carlos Gonzalez <homearanya@gmail.com>
2018-11-14 21:25:15 +0200

    netlify identity widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         `gatsby-plugin-netlify-cms`,
+        'gatsby-plugin-netlify-identity-widget',
         {
             resolve: `gatsby-plugin-layout`,
             options: {
