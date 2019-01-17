1a7dc619fa2ca89acba4253b8545e9d7b7fd7fb1
Carlos Gonzalez <homearanya@gmail.com>
2019-01-17 11:09:24 +0200

    implementing netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    plugins: [
-        `gatsby-plugin-netlify-cms`,
-        'gatsby-plugin-netlify-identity-widget',
-        {
-            resolve: `gatsby-plugin-layout`,
-            options: {
-                component: require.resolve(`./src/components/Layout`)
-            }
-        }
-    ]
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/Layout`)
+      }
+    },
+    "gatsby-plugin-netlify-identity-widget",
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
 };
