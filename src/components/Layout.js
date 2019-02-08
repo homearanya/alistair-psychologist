94f114ba41b0b221bed3455b11c66d0802b0ec1a
Carlos Gonzalez <homearanya@gmail.com>
2019-02-08 23:56:32 +0200

    playing with performance

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -48,12 +48,12 @@ export default function Layout(props) {
                 content="5Fs1mwvNeUdz1y6CfK5miXOOFUra094G_nhpRiVyXXQ"
               /> */}
               {/* Font Awesome */}
-              <script
+              {/* <script
                 defer
                 src="https://use.fontawesome.com/releases/v5.7.1/js/all.js"
                 integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7"
                 crossorigin="anonymous"
-              />
+              /> */}
             </Helmet>
 
             <div id="canvas">
