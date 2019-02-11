1d3b32b4155a64bc5e60aa7e50f1866e3094f91d
Carlos Gonzalez <homearanya@gmail.com>
2019-02-11 13:14:41 +0200

    remove react-reveal from contact page

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Zoom from "react-reveal/Zoom";
+// import Zoom from "react-reveal/Zoom";
 import Layout from "../components/Layout";
 import Breadcrumbs from "../components/Breadcrumbs";
 import { ContactForm } from "../components/ContactForm";
@@ -23,23 +23,23 @@ export default ({ data }) => {
         pages={pages}
       />
 
-      <Zoom>
-        <section className="ls columns_padding_25 section_padding_top_100 section_padding_bottom_100">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-7">
-                <DynamicAnchor id="contact-page" />
-                <h3 className="module-header">Contact Form</h3>
-                <ContactForm className="contact-form row columns_margin_bottom_40" />
-              </div>
+      {/* <Zoom> */}
+      <section className="ls columns_padding_25 section_padding_top_100 section_padding_bottom_100">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <DynamicAnchor id="contact-page" />
+              <h3 className="module-header">Contact Form</h3>
+              <ContactForm className="contact-form row columns_margin_bottom_40" />
+            </div>
 
-              <div className="col-md-5">
-                <ContactDetails2 />
-              </div>
+            <div className="col-md-5">
+              <ContactDetails2 />
             </div>
           </div>
-        </section>
-      </Zoom>
+        </div>
+      </section>
+      {/* </Zoom> */}
     </Layout>
   );
 };
