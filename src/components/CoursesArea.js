078987a290573ecb75ba7eeeeff8b85f441e2a79
Carlos Gonzalez <homearanya@gmail.com>
2019-02-13 09:11:29 +0200

    improving code reusability

--- src/components/CoursesArea.js
+++ src/components/CoursesArea.js
@@ -1,80 +1,23 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
 
 import UpcomingCourse from "./UpcomingCourse2";
+import FilteredCourses from "./FilteredCourses";
 
 export default function CoursessArea(props) {
   return (
-    <StaticQuery
-      query={graphql`
-        query UpcomingCoursesAreaQuery {
-          allMarkdownRemark(
-            limit: 3
-            sort: { order: ASC, fields: [frontmatter___dateStart] }
-            filter: { frontmatter: { templateKey: { eq: "upcoming-courses" } } }
-          ) {
-            edges {
-              node {
-                fields {
-                  uCourseMTCourses {
-                    fields {
-                      slug
-                    }
-                  }
-                }
-                html
-                frontmatter {
-                  courseName
-                  dateStart
-                  dateEnd
-                  venue
-                  thumbnailimage {
-                    image {
-                      childImageSharp {
-                        fluid(maxWidth: 500) {
-                          ...GatsbyImageSharpFluid
-                        }
-                      }
-                    }
-                    alt
-                  }
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        const { edges: courses } = data.allMarkdownRemark;
-        return (
-          // <section className="ls section_padding_top_130 section_padding_bottom_100 columns_margin_top_0 columns_margin_bottom_30">
-          <section className="ls section_padding_top_130 columns_margin_top_0 columns_margin_bottom_30">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-12 text-center">
-                  <h2 className="section_header ">
-                    {props.coursesArea.heading}
-                  </h2>
-                  <p>{props.coursesArea.blurb}</p>
-                </div>
-              </div>
-              <div className="row mosaic-post">
-                {courses.map(({ node: course }, index) => (
-                  <UpcomingCourse
-                    key={index}
-                    frontmatter={course.frontmatter}
-                    html={course.html}
-                    courseSlug={`${
-                      course.fields.uCourseMTCourses.fields.slug
-                    }#start-content`}
-                    siteUrl={props.siteUrl}
-                  />
-                ))}
-              </div>
-            </div>
-          </section>
-        );
-      }}
-    />
+    // <section className="ls section_padding_top_130 section_padding_bottom_100 columns_margin_top_0 columns_margin_bottom_30">
+    <section className="ls section_padding_top_130 columns_margin_top_0 columns_margin_bottom_30">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <h2 className="section_header ">{props.coursesArea.heading}</h2>
+            <p>{props.coursesArea.blurb}</p>
+          </div>
+        </div>
+        <div className="row mosaic-post">
+          <FilteredCourses upcomingCourse={UpcomingCourse} />
+        </div>
+      </div>
+    </section>
   );
 }
--- src/components/UpcomingCourses.js
+++ src/components/UpcomingCourses.js
@@ -1,97 +1,19 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
-import { Location } from "@reach/router";
 
 import UpcomingCourse from "./UpcomingCourse";
+import FilteredCourses from "./FilteredCourses";
 
 export default function UpcomingCourses() {
   return (
-    <StaticQuery
-      query={graphql`
-        query UpcomingCoursesQuery {
-          allMarkdownRemark(
-            sort: { order: ASC, fields: [frontmatter___dateStart] }
-            filter: { frontmatter: { templateKey: { eq: "upcoming-courses" } } }
-          ) {
-            edges {
-              node {
-                fields {
-                  uCourseMTCourses {
-                    fields {
-                      slug
-                    }
-                  }
-                }
-                html
-                frontmatter {
-                  courseName
-                  dateStart
-                  dateEnd
-                  venue
-                  thumbnailimage {
-                    image {
-                      childImageSharp {
-                        fluid(maxWidth: 500) {
-                          ...GatsbyImageSharpFluid
-                        }
-                      }
-                    }
-                    alt
-                  }
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        const today = new Date();
-        const { edges: upcomingCourses } = data.allMarkdownRemark;
-        return (
-          // <section className="ls section_padding_top_100 section_padding_bottom_100 columns_padding_25">
-          <Location>
-            {({ location }) => (
-              <section>
-                <div className="container">
-                  <div className="row mosaic-post">
-                    {/* <div className="col-sm-10 col-sm-push-1"> */}
-                    <div>
-                      {upcomingCourses.reduce(
-                        (upcomingCourses, upcomingCourse, index) => {
-                          const {
-                            fields,
-                            html,
-                            frontmatter
-                          } = upcomingCourse.node;
-                          const courseDate = new Date(
-                            upcomingCourse.node.frontmatter.dateStart
-                          );
-                          // filter out expired courses
-                          if (courseDate > today) {
-                            upcomingCourses.push(
-                              <UpcomingCourse
-                                key={index}
-                                frontmatter={frontmatter}
-                                html={html}
-                                courseSlug={`${
-                                  fields.uCourseMTCourses.fields.slug
-                                }#start-content`}
-                                siteUrl={location.origin}
-                              />
-                            );
-                          }
-                          return upcomingCourses;
-                        },
-                        []
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </section>
-            )}
-          </Location>
-        );
-      }}
-    />
+    <section>
+      <div className="container">
+        <div className="row mosaic-post">
+          {/* <div className="col-sm-10 col-sm-push-1"> */}
+          <div>
+            <FilteredCourses upcomingCourse={UpcomingCourse} />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
