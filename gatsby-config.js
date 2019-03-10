4efd768408d1c6b39fdb1848b85021c14d95e0e6
Carlos Gonzalez <homearanya@gmail.com>
2019-03-10 15:59:33 +0200

    install Google Tag Manager, robots and npm update

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,28 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://www.alistairmork-chadwick.co.za",
+        sitemap: "https://www.alistairmork-chadwick.co.za/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-P7T4MLS",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+      }
+    },
+    {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
