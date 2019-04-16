7edc423f848014f275cb5aacd2fb3e02f2de0b7d
Carlos Gonzalez <homearanya@gmail.com>
2019-04-16 08:34:41 +0200

    playing with custom previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,5 @@
 import CMS from "netlify-cms";
 
-import "../assets/css/bootstrap.min.css";
-import "../assets/css/main.css";
-import "../assets/css/custom.css";
-
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import HomePagePreview from "./preview-templates/HomePagePreview";
 
