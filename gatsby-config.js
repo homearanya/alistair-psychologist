281369c390cf0ba47cfd9e4040a2dae41065223e
Carlos Gonzalez <homearanya@gmail.copm>
2022-07-28 11:45:53 +0200

    remove gatsby-source-filesystem path

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,13 +83,6 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/static/img`,
-        name: "uploads",
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
         path: `${__dirname}/src/general`,
         name: "general",
       },
